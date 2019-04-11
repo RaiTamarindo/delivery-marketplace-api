bab9bc3df61be0d3ce2e6b9c9855391556e9f930
Raí Tamarindo <rai.tamarindo@banksystem.com.br>
2019-04-11 11:02:38 -0300

    Turned user nullable on addresses

---
+++ database/migrations/2019_04_11_000001_turn_user_nullable_addresses.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Schema\Blueprint;
+
+Capsule::schema()->table('addresses', function(Blueprint $table) {
+    $table->unsignedBigInteger('user_id')->nullable()->change();
+});
