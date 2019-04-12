ab7d06dcc081d66581fd9b94385a2cd7bd92fd8f
Raí Tamarindo <raitamarindo@gmail.com>
2019-04-11 22:29:35 -0300

    Fixed email validation

--- src/Middleware/UserValidatorMiddleware.php
+++ src/Middleware/UserValidatorMiddleware.php
@@ -9,7 +9,10 @@ class UserValidatorMiddleware extends ValidatorMiddleware {
     protected function getRules(): Rules\AllOf {
         return new Rules\AllOf(
             new Rules\Key('name', new Rules\Length(1, 255)),
-            new Rules\Key('email', new Rules\Length(1, 255)),
+            new Rules\Key('email', new Rules\AllOf(
+                new Rules\Length(1, 255),
+                new Rules\Email(),
+            )),
             new Rules\Key('password', new Rules\Length(10, 32)),
         );
     }
